7a3f132a17a73d7de74952ec1b0ec0569855eeb5
zzoPark <rweee408@gmail.com>
2020-01-26 17:19:37 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const { removeDups } = require('@zzopark/common-utils')
+
+exports.required = (v) => !!v || 'Required.'
+
+exports.notAllowed = (regex) => (v) => {
+  const matches = v.match(regex)
+  if (matches === null) return true
+  return `Not allowed characters ${removeDups(matches)}`
+}
+
+exports.noSpaces = (v) => {
+  const regex = /[\s]+/
+  return regex.test(v) || 'No spaces.'
+}
+
+exports.minLength = (min) => (v) => (
+  v.length >= min || `Need more than ${min} characters.`
+)
+
+exports.notStartWith = (s) => (v) => (
+  !v.startsWith(s) || `Cannot start with "${s}".`
+)
+
+exports.notEndWith = (s) => (v) => (
+  !v.endsWith(s) || `Cannot end with "${s}".`
+)
